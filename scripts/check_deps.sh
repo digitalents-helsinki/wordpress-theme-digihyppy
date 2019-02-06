d37081c954e82e0b14f1c39221c13f275023525f
Joonas Yliaho <joonas.yliaho@gmail.com>
2019-02-06 11:36:50 +0200

    1 exit on missing software deps

--- scripts/check_deps.sh
+++ scripts/check_deps.sh
@@ -4,20 +4,23 @@
 which -a yarn
 if [ "$?" -ne 0 ]; then
   echo -e "${RED}Yarn not found. Exiting...${NC}"
-
+  exit 1
 fi
 
 which -a npm
 if [ "$?" -ne 0 ]; then
   echo -e "${RED}npm not found. Exiting...${NC}"
+  exit 1
 fi
 
 which -a composer
 if [ "$?" -ne 0 ]; then
   echo -e "${RED}Composer not found. Exiting...${NC}"
+  exit 1
 fi
 
 which -a docker-compose
 if [ "$?" -ne 0 ]; then
   echo -e "${RED}docker-compose not found. Exiting...${NC}"
+  exit 1
 fi
